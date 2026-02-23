b877ae15c73a316c5a0952da3fd5d5df259028a4
Junak12 <pauljunak@gmail.com>
2026-02-23 17:07:40 +0600

    Correct home page in terms of view details button in next to Cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,10 @@ import JobCards from "../../components/CardSix/JobCards";
 import ReviewForm from "../../components/Reviews/ReviewForm";
 import TopReviews from "../../components/Reviews/TopReviews";
 import { useData } from "../../hooks/useData";
+import { useNavigate } from "react-router";
 
 const Home = () => {
+  const navigate = useNavigate();
   const { data, loading } = useData();
   const jobs = data.slice(0,6);
   useEffect(() => {
@@ -35,12 +37,21 @@ const Home = () => {
             </div>
           ))}
         </div>
+        <div className="flex items-center justify-center mt-6 lg:mt-6">
+          <button
+            className="text-xl border px-6 py-2 rounded-2xl bg-cyan-700 text-white font-semibold
+          cursor-pointer hover:scale-110 transition-all"
+            onClick={() => navigate("/AllJob")}
+          >
+            View More
+          </button>
+        </div>
       </div>
       <div className="mx-auto mt-15">
-        <ReviewForm/>
+        <ReviewForm />
       </div>
       <div className="mt-10 mb-10">
-        <TopReviews/>
+        <TopReviews />
       </div>
     </div>
   );
